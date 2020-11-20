600abf7d7f8767762c6cfcc5d91c6b4b3495ef16
Rafael Garcia <rafagrcb@gmail.com>
2020-11-20 00:58:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,17 +6,26 @@ class Student
 
   def initialize(student_hash)
     
+    student_hash.each {|k,v| self.send(("#{k}="),v)}
+    @@all << self
+    
   end
 
   def self.create_from_collection(students_array)
+    
+    students_array.each{|student| Student.new(student)}
     
   end
 
   def add_student_attributes(attributes_hash)
     
+    attributes_hash.each{|k,v| self.send(("#{k}="),v)}
+    
   end
 
   def self.all
+    
+    @@all
     
   end
 end
